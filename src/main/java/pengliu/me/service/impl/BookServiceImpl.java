b17bedbe5042da5f8099059d87c1b66b4eacb929
liupeng330 <330liupeng@gmail.com>
2015-09-03 00:09:34 +0800

    adding trasction

--- src/main/java/pengliu/me/service/impl/BookServiceImpl.java
+++ src/main/java/pengliu/me/service/impl/BookServiceImpl.java
@@ -1,10 +1,12 @@
 package pengliu.me.service.impl;
 
+import org.springframework.transaction.annotation.Transactional;
 import pengliu.me.dao.BookDao;
 import pengliu.me.model.BookEntity;
 import pengliu.me.service.BookService;
 
 //业务逻辑的实现类
+@Transactional
 public class BookServiceImpl implements BookService
 {
     private BookDao bookDao;
@@ -15,6 +17,11 @@ public class BookServiceImpl implements BookService
 
     public int addBook(BookEntity book)
     {
-        return (Integer) bookDao.save(book);
+        int ret = (Integer) bookDao.save(book);
+//        if(ret > 0)
+//        {
+//            throw new RuntimeException("test for transational");
+//        }
+        return ret;
     }
 }
